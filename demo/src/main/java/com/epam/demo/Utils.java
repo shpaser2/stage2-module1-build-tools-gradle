1c8a1d059b656b0589078ecc64b9a3d3d6aa6728
Siarhei_Shpakouski <siarhei_shpakouski@epam.com>
2023-05-25 07:04:46 +0400

    add impl to build and settings gradle, java classes

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,17 @@
 package com.epam.demo;
 
+import com.epam.utils.StringUtils;
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        //magic happens here
+        boolean allArePositive = false;
+        for (String s : args) {
+            allArePositive = StringUtils.isPositiveNumber(s);
+            if (!allArePositive) {
+                break;
+            }
+        }
+        return allArePositive;
     }
 }
